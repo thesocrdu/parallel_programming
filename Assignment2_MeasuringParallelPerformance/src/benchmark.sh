a69edb620f5480507180f70c529532a6a3d140ff
Kyle <kyle@kyle-VirtualBox.(none)>
2013-03-06 00:34:24 -0500

    Editied benchmark with more realistic iteration parameters

--- Assignment2_MeasuringParallelPerformance/src/benchmark.sh
+++ Assignment2_MeasuringParallelPerformance/src/benchmark.sh
@@ -5,7 +5,7 @@ for (( threads = 1; threads <= 16; threads = $((threads+1)) ))
 do
     echo "Num threads = $threads"
 
-    for(( flips = 100000; flips <= 1600000; flips =$((flips+100000)) ))
+    for(( flips = 100000000; flips <= 1600000000; flips =$((flips+100000000)) ))
     do
         echo "Now doing $flips flips."
         java ParallelCoinFlip $threads $flips >> log_coin.txt
@@ -20,7 +20,7 @@ for (( threads = 1; threads <= 16; threads = $((threads+1)) ))
 do
     echo "Num threads = $threads"
 
-    for(( bitSize = 15; bitSize <= 30; bitSize =$((bitSize+1)) ))
+    for(( bitSize = 15; bitSize <= 25; bitSize =$((bitSize+1)) ))
     do
         echo "Now decrypting $bitSize bits."
         java SealedDESParallel $threads $bitSize >> log_Sealed_DES.txt
